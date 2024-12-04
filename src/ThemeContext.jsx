5df7fee98d2c519209867db6a83e224b19a56b5a
PrashanthiVelpula <prashanthivelpula2000@gmail.com>
2024-12-04 17:03:38 -0500

    Updated dark theme state

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -1,15 +1,33 @@
 // src/ThemeContext.js
 
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext,useEffect } from 'react';
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
     const [isDarkTheme, setIsDarkTheme] = useState(false);
 
+    useEffect(() => {
+        const savedTheme = sessionStorage.getItem('theme');
+        if (savedTheme) {
+            setIsDarkTheme(savedTheme === 'dark');
+        }
+    }, []);
+
+    // Toggle the theme and save to localStorage
     const toggleTheme = () => {
-        setIsDarkTheme((prevTheme) => !prevTheme);
+        setIsDarkTheme(prevTheme => {
+            const newTheme = !prevTheme;
+            sessionStorage.setItem('theme', newTheme ? 'dark' : 'light');
+            return newTheme;
+        });
     };
+
+
+
+    // const toggleTheme = () => {
+    //     setIsDarkTheme((prevTheme) => !prevTheme);
+    // };
 
     return (
         <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
